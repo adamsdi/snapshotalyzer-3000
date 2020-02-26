ff5782df1d087c8710832c2201b7bbd1ee49d2d1
Diamond <diamondadams7@gmail.com>
2020-02-25 23:00:01 -0500

    List volumes and snapshots. Naive implementation of snapshot command

--- README.md
+++ README.md
@@ -15,4 +15,8 @@ shotty uses the configuration file created by the AWS cli. e.g.
 
 ## Running
 
-'pipenv run "python shotty/shotty.py"''
+'pipenv run "python shotty/shotty.py <command> <subcommand> <--project=PROJECT>"''
+
+*command* is instances, volumes, or snapshots
+*subcommand* - depends on command
+*project* is optional
--- shotty/shotty.py
+++ shotty/shotty.py
@@ -17,10 +17,85 @@ def filter_instances(project):
     return instances
 
 @click.group()
+def cli():
+    """Shotty manages snapshots"""
+
+
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 snapshots"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(", ".join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+
+    return
+
+
+
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(", ".join((
+            v.id,
+            i.id,
+            v.state,
+            str(v.size) + "GiB",
+            v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+
+    return
+
+@cli.group('instances')
 def instances():
     """Commands for instances"""
 
-@instances.command('start')
+@instances.command('snapshot',
+    help="Create snapshots of all volumes")
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def create_snapshots(project):
+    """Create snapshots for EC2 instances"""
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        i.stop()
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshots(Description="Created by Snapshotalyzer 3000")
+
+    return
+
+
+@instances.command('list')
 @click.option('--project', default=None,
     help="Only instances for project (tag Project:<name>)")
 def list_instances(project):
@@ -43,7 +118,7 @@ def list_instances(project):
 @instances.command('start')
 @click.option('--project', default=None,
 help='Only isntances for project')
-def stop_instances(project):
+def start_instances(project):
     "Start EC2 instances"
 
     instances = []
@@ -60,5 +135,25 @@ def stop_instances(project):
 
     return
 
+@instances.command('stop')
+@click.option('--project', default=None,
+help='Only isntances for project')
+def stop_instances(project):
+    "Stop EC2 instances"
+
+    instances = []
+
+    if project:
+        filters = [{'Name': 'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+
+    return
+
 if __name__ == '__main__':
-    instances()
+    cli()
